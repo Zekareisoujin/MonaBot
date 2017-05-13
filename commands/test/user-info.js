0b91931fa69d6ae551c5b9f559df5711956e09b6
Zekareisoujin <Zeka304@gmail.com>
2017-05-13 18:23:36 +0800

    Switch to discord commando

---
+++ commands/test/user-info.js
@@ -0,0 +1,43 @@
+const commando = require('discord.js-commando');
+const RichEmbed = require('discord.js').RichEmbed;
+
+const argKey = 'user-info';
+
+module.exports = class UserCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            'name': 'user-info',
+            'aliases': [
+                'user',
+                'u'
+            ],
+            'group': 'test',
+            'memberName': 'user-info',
+            'description': 'Display user info.',
+            'example': [
+                'user Nad#4039',
+                'u Nad'
+            ],
+            'args': [
+                {
+                    'key': argKey,
+                    'label': 'user',
+                    'prompt': 'Specify an user.',
+                    'type': 'member',
+                }
+            ]
+        });
+    }
+
+    async run(msg, args) {
+        const member = args[argKey];
+        const user = member.user;
+        const embed = new RichEmbed()
+            .setAuthor(user.username)
+            .setThumbnail(user.avatarURL)
+            .addField('Joined date', new Date(member.joinedTimestamp));
+        
+        if (member.nickname) embed.addField('Nick name', member.nickname);
+        return msg.channel.send({embed});
+    }
+}
--- ping.js
+++ ping.js
@@ -3,16 +3,20 @@
 */
 
 // Import the discord.js module
-const Discord = require('discord.js');
+const Discord = require('discord.js-commando');
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-// File reader to read app token
-const fs = require('fs');
-
-// // The token of your bot - https://discordapp.com/developers/applications/me
-// const token = '';
+let auth
+try {
+  auth = require('./auth.json')
+} catch (err) {
+  auth = {
+    token: process.env['TOKEN'],
+    admins: (process.env['ADMINS'] || '').split(',')
+  }
+}
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -26,13 +30,8 @@ client.on('message', message => {
     if (message.content === 'ping') {
         // Send "pong" to the same channel
         message.channel.send('pong');
+        console.log(message.author);
     }
 });
 
-fs.readFile('token', (err, data) => {
-    if (err) throw err;
-    var token = (new String(data)).toString();
-
-    // Log our bot in
-    client.login(token);
-});
+client.login(auth.token);
