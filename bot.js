610bcfe56f8fef1d2d0b097eabf55703d1ef5fca
Zekareisoujin <Zeka304@gmail.com>
2017-06-01 11:34:50 +0800

    Set bot game status

--- bot.js
+++ bot.js
@@ -29,6 +29,7 @@ client
     .on('debug', console.log)
     .on('ready', () => {
         console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+        client.user.setGame('You must be tired after today | Let’s go to sleep.')
     })
     .on('disconnect', () => { console.warn('Disconnected!'); })
     .on('reconnecting', () => { console.warn('Reconnecting...'); })
